7f3b6bfb9369568eea00a08bbef166e07a1f056b
liting <luz.liting@gmail.com>
2022-12-31 15:09:26 +0800

    refactor: rm pinceau

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,13 @@
  * @Date: 2022-11-21 19:54:30
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2022-12-31 14:04:04
+ * @LastEditTime: 2022-12-31 15:08:49
  * @FilePath: /nuxt-theme-liting/nuxt.config.ts
  */
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: [
-    // https://content.nuxtjs.org
-    '@nuxt/content',
-    // https://github.com/Tahul/pinceau
-    'pinceau/nuxt',
-  ],
-
+  modules: ['@nuxt/content'],
   content: {
     documentDriven: true,
-  },
-
-  pinceau: {
-    configFileName: 'tokens.config',
   },
 })
--- tokens.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineTheme, palette } from 'pinceau'
-
-const primary = palette('#0070f3')
-
-export default defineTheme({
-  colors: {
-    primary,
-  },
-})
