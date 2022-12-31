ad4bad59b9d149a1349f35b8a647d7178d1feb39
liting <luz.liting@gmail.com>
2022-12-31 15:25:30 +0800

    chore: add @unocss/nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,17 @@
  * @Date: 2022-11-21 19:54:30
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2022-12-31 15:08:49
+ * @LastEditTime: 2022-12-31 15:24:35
  * @FilePath: /nuxt-theme-liting/nuxt.config.ts
  */
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: ['@nuxt/content', '@unocss/nuxt'],
   content: {
     documentDriven: true,
+  },
+  unocss: {
+    uno: true,
+    icons: true,
+    attributify: true,
   },
 })
