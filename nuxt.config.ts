8e4e77889f62c6cf9831ea7ee4f2c437b54cc8f9
liting <luz.liting@gmail.com>
2022-12-31 15:39:35 +0800

    chore: add @nuxtjs/color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,11 @@
  * @Date: 2022-11-21 19:54:30
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2022-12-31 15:24:35
+ * @LastEditTime: 2022-12-31 15:38:56
  * @FilePath: /nuxt-theme-liting/nuxt.config.ts
  */
 export default defineNuxtConfig({
-  modules: ['@nuxt/content', '@unocss/nuxt'],
+  modules: ['@nuxt/content', '@unocss/nuxt', '@nuxtjs/color-mode'],
   content: {
     documentDriven: true,
   },
@@ -14,5 +14,15 @@ export default defineNuxtConfig({
     uno: true,
     icons: true,
     attributify: true,
+  },
+  colorMode: {
+    preference: 'system',
+    fallback: 'light',
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storageKey: 'nuxt-color-mode',
   },
 })
