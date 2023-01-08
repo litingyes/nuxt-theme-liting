a3287b0a823ffbf7b84fcfb967b9a1e59a181046
liting <luz.liting@gmail.com>
2023-01-08 21:09:08 +0800

    perf: add padding-left for ul/ol el

--- components/content/ProseOl.vue
+++ components/content/ProseOl.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Date: 2023-01-05 23:07:57
+ * @Author: liting luz.liting@gmail.com
+ * @LastEditors: liting luz.liting@gmail.com
+ * @LastEditTime: 2023-01-08 21:08:27
+ * @FilePath: /nuxt-theme-liting/components/content/ProseOl.vue
+-->
 <template>
-  <ol>
+  <ol class="ml-4">
     <slot />
   </ol>
 </template>
--- components/content/ProseUl.vue
+++ components/content/ProseUl.vue
@@ -1,5 +1,12 @@
+<!--
+ * @Date: 2023-01-05 23:07:57
+ * @Author: liting luz.liting@gmail.com
+ * @LastEditors: liting luz.liting@gmail.com
+ * @LastEditTime: 2023-01-08 21:07:48
+ * @FilePath: /nuxt-theme-liting/components/content/ProseUl.vue
+-->
 <template>
-  <ul>
+  <ul class="ml-4">
     <slot />
   </ul>
 </template>
