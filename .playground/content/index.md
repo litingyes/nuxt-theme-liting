3dc38185b361d0cdd50c3f60144efac2e78e5322
liting <luz.liting@gmail.com>
2023-01-15 01:11:18 +0800

    refactor(/index): home layout
    rm homePage sfc
    
    BREAKING CHANGE: perf config of home page

--- .playground/content/index.md
+++ .playground/content/index.md
@@ -1,16 +1,10 @@
 --- 
 layout: home
----
-
-::home-page
----
 avatar: /avatar.jpg
-home-cover: /home-cover.jpg
-test: 1
+cover: /home-cover.jpg
 social:
   twitter: https://twitter.com/liting_yes
   blibli: https://space.bilibili.com/472116791
   zhihu: https://www.zhihu.com/people/liu-mou-mou-94-79
   github: https://github.com/liting-yes
 ---
-::
--- layouts/home.vue
+++ layouts/home.vue
@@ -2,11 +2,35 @@
  * @Date: 2023-01-02 10:01:12
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-02 10:55:06
+ * @LastEditTime: 2023-01-15 01:08:33
  * @FilePath: /nuxt-theme-liting/layouts/home.vue
 -->
+
+<script lang="ts" setup>
+const { page } = useContent()
+</script>
+
 <template>
   <div class="home-layout">
-    <slot />
+    <div
+      class="h-screen flex flex-col items-center pt-48 bg-cover dark:grayscale"
+      :style="{ backgroundImage: `url(${page.cover})` }"
+    >
+      <LitingAvatar :url="page.avatar" size="large" round></LitingAvatar>
+      <div class="flex items-center gap-2 mt-4">
+        <NuxtLink v-if="page.social.twitter" :to="page.social.twitter" target="_blank">
+          <div class="i-ri:twitter-line interact-btn text-2xl"></div>
+        </NuxtLink>
+        <NuxtLink v-if="page.social.blibli" :to="page.social.blibli" target="_blank">
+          <div class="i-liting:blibli interact-btn text-2xl"></div>
+        </NuxtLink>
+        <NuxtLink v-if="page.social.zhihu" :to="page.social.zhihu" target="_blank">
+          <div class="i-ri:zhihu-line interact-btn text-2xl"></div>
+        </NuxtLink>
+        <NuxtLink v-if="page.social.github" :to="page.social.github" target="_blank">
+          <div class="i-mdi:github interact-btn text-2xl"></div>
+        </NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
