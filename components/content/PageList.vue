e9c72368300139bd9eb83c8a1da0ac99ef0cf9bc
liting <luz.liting@gmail.com>
2023-01-15 01:14:37 +0800

    perf(PageList): form of center layout

--- components/content/PageList.vue
+++ components/content/PageList.vue
@@ -2,8 +2,8 @@
  * @Date: 2023-01-14 22:58:39
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-15 00:57:22
- * @FilePath: /nuxt-theme-liting/components/content/ArticleList.vue
+ * @LastEditTime: 2023-01-15 01:12:55
+ * @FilePath: /nuxt-theme-liting/components/content/PageList.vue
 -->
 <script lang="ts" setup>
 interface Props {
@@ -39,8 +39,12 @@ const { lastUpdateTime } = useThemeConfig().value
       limit: props.limit,
     }"
   >
-    <ul class="w-xl list-none" v-bind="$attrs">
-      <li v-for="page in filterList(list)" :key="page._path" class="my-4 shadow-md bg-emerald-200 rounded-lg dark:bg-slate-900">
+    <ul class="list-none" v-bind="$attrs">
+      <li
+        v-for="page in filterList(list)"
+        :key="page._path"
+        class="my-4 mx-auto w-xl shadow-md bg-emerald-200 rounded-lg dark:bg-slate-900"
+      >
         <NuxtLink class="block px-6 py-4" :to="page._path">
           <h2 class="text-base font-semibold mb-2">{{ page.title ?? '标题为空' }}</h2>
           <p v-if="page.description" class="text-sm my-2">{{ page.description }}</p>
