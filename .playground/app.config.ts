ae5545855ff1cf42deec682b4a57de123494325c
liting <luz.liting@gmail.com>
2023-01-15 19:52:54 +0800

    refactor(app config): move config from root dir to doc dir

--- .playground/app.config.ts
+++ .playground/app.config.ts
@@ -6,5 +6,53 @@
  * @FilePath: /nuxt-theme-liting/.playground/app.config.ts
  */
 export default defineAppConfig({
-  theme: {},
+  theme: {
+    title: 'Nuxt Theme Liting',
+    description: 'a theme about nuxt',
+    search: false,
+    nav: {
+      timeline: true,
+      tag: true,
+      extra: true,
+      icon: {
+        skin: true,
+        i18n: false,
+        github: {
+          disabled: false,
+          url: 'https://github.com/liting-yes/nuxt-theme-liting.git',
+        },
+      },
+    },
+    sidebar: [
+      {
+        text: '开始',
+        items: [
+          {
+            text: '主题介绍',
+            path: '/start/instro',
+          },
+          {
+            text: '主题配置',
+            path: '/start/config',
+          },
+          {
+            text: '文本编辑',
+            path: '/start/md',
+          },
+          {
+            text: 'Git使用指南',
+            path: '/start/git',
+          },
+          {
+            text: '站点部署指南',
+            path: '/start/deploy',
+          },
+        ],
+      },
+    ],
+    lastUpdateTime: {
+      text: '最近更新时间',
+      format: 'yyyy-MM-dd hh:mm',
+    },
+  },
 })
--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,7 @@
  * @Date: 2022-11-21 19:54:30
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-15 19:45:24
+ * @LastEditTime: 2023-01-15 19:51:11
  * @FilePath: /nuxt-theme-liting/app.config.ts
  */
 export default defineAppConfig({
@@ -11,9 +11,9 @@ export default defineAppConfig({
     description: 'a theme about nuxt',
     search: false,
     nav: {
-      timeline: true,
-      tag: true,
-      extra: true,
+      timeline: false,
+      tag: false,
+      extra: false,
       icon: {
         skin: true,
         i18n: false,
@@ -23,37 +23,8 @@ export default defineAppConfig({
         },
       },
     },
-    sidebar: [
-      {
-        text: '开始',
-        items: [
-          {
-            text: '主题介绍',
-            path: '/start/instro',
-          },
-          {
-            text: '主题配置',
-            path: '/start/config',
-          },
-          {
-            text: '文本编辑',
-            path: '/start/md',
-          },
-          {
-            text: 'Git使用指南',
-            path: '/start/git',
-          },
-          {
-            text: '站点部署指南',
-            path: '/start/deploy',
-          },
-        ],
-      },
-    ],
-    lastUpdateTime: {
-      text: '最近更新时间',
-      format: 'yyyy-MM-dd hh:mm',
-    },
+    sidebar: [],
+    lastUpdateTime: false,
   },
 })
 
