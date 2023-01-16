1465079a98929ace57a3819540f85ce8b0dcabda
liting <luz.liting@gmail.com>
2023-01-16 12:48:42 +0800

    feat(extra layout): support responsive desgin

--- layouts/extra.vue
+++ layouts/extra.vue
@@ -2,7 +2,7 @@
  * @Date: 2023-01-15 18:17:15
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-15 19:27:35
+ * @LastEditTime: 2023-01-16 12:46:53
  * @FilePath: /nuxt-theme-liting/layouts/extra.vue
 -->
 <script lang="ts" setup>
@@ -11,7 +11,7 @@ const { page } = useContent()
 
 <template>
   <div class="extra-layout pt-16">
-    <div class="w-3xl mx-auto mt-16">
+    <div class="max-w-3xl mx-auto mt-16 px-12 sm:px-0">
       <div v-for="extraGroup in page.extra" :key="extraGroup.text">
         <h2 class="mt-8 mb-4">{{ extraGroup.text }}</h2>
         <ul class="list-none grid grid-cols-2">
