d6b481dd4fcae789127dc1ffbf65a42f4372c677
liting <luz.liting@gmail.com>
2023-01-17 16:04:48 +0800

    perf(PageList): add padding-bottm

--- layouts/tag.vue
+++ layouts/tag.vue
@@ -2,7 +2,7 @@
  * @Date: 2023-01-15 11:47:08
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-17 15:27:39
+ * @LastEditTime: 2023-01-17 16:03:21
  * @FilePath: /nuxt-theme-liting/layouts/tag.vue
 -->
 <script lang="ts" setup>
@@ -78,6 +78,6 @@ const selectedTag = ref(tags.value[0])
         <span>{{ tag.tag }} {{ tag.total }}</span></li
       >
     </ul>
-    <LitingPageList class="!px-0 !sm:px-8" :list="pagesData[selectedTag].list"></LitingPageList>
+    <LitingPageList class="!px-0 !sm:px-8 pb-4" :list="pagesData[selectedTag].list"></LitingPageList>
   </div>
 </template>
--- layouts/timeline.vue
+++ layouts/timeline.vue
@@ -2,7 +2,7 @@
  * @Date: 2023-01-14 13:10:49
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-17 15:13:17
+ * @LastEditTime: 2023-01-17 16:03:01
  * @FilePath: /nuxt-theme-liting/layouts/timeline.vue
 -->
 <script lang="ts" setup>
@@ -11,6 +11,6 @@ const { disabled, currentLocale } = useLocale()
 
 <template>
   <div class="timeline-layout pt-24">
-    <LitingPageList :path="disabled ? '/' : currentLocale?.prefix"></LitingPageList>
+    <LitingPageList class="pb-4" :path="disabled ? '/' : currentLocale?.prefix"></LitingPageList>
   </div>
 </template>
