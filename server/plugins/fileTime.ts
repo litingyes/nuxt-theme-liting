567719b628c743e0b2e2b4ecb3c5e452bbba3117
liting <luz.liting@gmail.com>
2023-01-18 11:31:43 +0800

    test(git): rm git dep

--- server/plugins/fileTime.ts
+++ server/plugins/fileTime.ts
@@ -2,20 +2,24 @@
  * @Date: 2023-01-08 22:04:56
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-17 15:36:22
+ * @LastEditTime: 2023-01-18 11:31:00
  * @FilePath: /nuxt-theme-liting/server/plugins/fileTime.ts
  */
-import path from 'path'
-import { execa } from 'execa'
+// import path from 'path'
+// import { execa } from 'execa'
 
-const contentDir = '.playground'
+// const contentDir = '.playground'
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('content:file:afterParse', async (file) => {
-    const filePath = path.resolve(contentDir, file._source, file._file)
-    const { stdout: unixAuthor } = await execa('git', ['log', '-1', '--pretty=format:"%at"', filePath])
-    const { stdout: unixCommitter } = await execa('git', ['log', '-1', '--pretty=format:"%ct"', filePath])
-    file.unixAuthor = parseInt(unixAuthor.slice(1, -1))
-    file.unixCommitter = parseInt(unixCommitter.slice(1, -1))
+    // const filePath = path.resolve(contentDir, file._source, file._file)
+    // const { stdout: unixAuthor } = await execa('git', ['log', '-1', '--pretty=format:"%at"', filePath])
+    // const { stdout: unixCommitter } = await execa('git', ['log', '-1', '--pretty=format:"%ct"', filePath])
+    // file.unixAuthor = parseInt(unixAuthor.slice(1, -1))
+    // file.unixCommitter = parseInt(unixCommitter.slice(1, -1))
+
+    // no git online
+    file.unixAuthor = Math.round(Date.now() / 1000)
+    file.unixCommitter = Math.round(Date.now() / 1000)
   })
 })
