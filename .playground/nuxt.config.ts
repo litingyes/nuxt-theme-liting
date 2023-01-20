64bb16602efaae3d1d3be1b3d338d98aae6b30b1
liting <luz.liting@gmail.com>
2023-01-20 12:48:03 +0800

    fix(locale): try to fix static generate file

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -2,12 +2,18 @@
  * @Date: 2022-11-21 19:54:30
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2022-12-31 13:56:51
+ * @LastEditTime: 2023-01-20 12:46:09
  * @FilePath: /nuxt-theme-liting/.playground/nuxt.config.ts
  */
+import { resolve } from 'path'
+import getRouteList from '../utils/routeList'
+
 export default defineNuxtConfig({
   extends: '..',
   typescript: {
     includeWorkspace: true,
+  },
+  generate: {
+    routes: getRouteList('**/*.md', { cwd: resolve(__dirname, './content') }),
   },
 })
