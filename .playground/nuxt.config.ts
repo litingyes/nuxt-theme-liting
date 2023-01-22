b1231856194d25d9b49365e3369ea92ac2c30f09
liting <luz.liting@gmail.com>
2023-01-23 01:08:27 +0800

    refactor(i18n): modify the method for add dynamic router
    when deploy on netlify/vercel, i18n is valid, try fix now
    
    BREAKING CHANGE: when deploy on netlify/vercel, i18n is valid, try fix now

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -2,12 +2,9 @@
  * @Date: 2022-11-21 19:54:30
  * @Author: liting luz.liting@gmail.com
  * @LastEditors: liting luz.liting@gmail.com
- * @LastEditTime: 2023-01-23 00:48:17
+ * @LastEditTime: 2023-01-23 01:03:50
  * @FilePath: /nuxt-theme-liting/.playground/nuxt.config.ts
  */
-import { resolve } from 'path'
-import getRouteList from '../utils/routeList'
-
 export default defineNuxtConfig({
   extends: '..',
   typescript: {
@@ -15,7 +12,17 @@ export default defineNuxtConfig({
   },
   nitro: {
     prerender: {
-      routes: [...getRouteList('**/*.md', { cwd: resolve(__dirname, './content') }), '/zh-cn'],
+      routes: [
+        '/zh-cn',
+        '/zh-cn/timeline',
+        '/zh-cn/tag',
+        '/zh-cn/extra',
+        '/zh-cn/start/instro',
+        '/zh-cn/start/config',
+        '/zh-cn/start/md',
+        '/zh-cn/start/git',
+        '/zh-cn/start/deploy',
+      ],
     },
   },
 })
